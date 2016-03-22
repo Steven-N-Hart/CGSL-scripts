2a90f179bd9d08c532852149d7c31ec7ee80f831
Jagadheshwar  Balan;Balan.Jagadheshwar@mayo.edu;16194403 <m139105@dlmpcim01.mayo.edu>
2016-03-22 11:50:07 -0500

    Changed to accept CLC vcf

--- Concordance/Concordance.sh
+++ Concordance/Concordance.sh
@@ -37,7 +37,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #This sets the default config file
 configFile=${DIR}/config/config.cfg
 OUTDIR=$PWD
-TIMESTAMP=$(date | cut -f3,4,2 -d " " | sed 's/ /_/g' | sed 's/:/_/g')
+TIMESTAMP=$(date | cut -f4,5,2 -d " " | sed 's/ /_/g' | sed 's/:/_/g')
 RESULTDIR="result"_$TIMESTAMP
 OUTDIRFLAG=0
 ##################################################################################
@@ -81,10 +81,8 @@ OUTDIR=$(readlink -f $OUTDIR)
 inputFile=`readlink -f $inputFile`
 cd $OUTDIR
 mkdir $OUTDIR/$RESULTDIR
-cp $inputFile $OUTDIR
+cp $inputFile $OUTDIR/$RESULTDIR/
 ConfigName=$(echo $inputFile | rev | cut -f1 -d '/' | rev)
-ConfigLoc=$OUTDIR/$ConfigName
-inputFile=$ConfigLoc	
 source $configFile
 #Ensure that there are 5 fields set
 INPUT_VALID=`awk -F'\t' '{if(NF!=5){print "Line number", NR,"is not set correctly in your input file\n"}}' $inputFile`
@@ -103,9 +101,54 @@ do
 	LABEL=$(awk -F'\t' -v var=$x '{if(NR==var){print $1}}' $inputFile)
 	VCF1=$(awk -F'\t' -v var=$x '{if(NR==var){print $2}}' $inputFile)
 	VCF2=$(awk -F'\t' -v var=$x '{if(NR==var){print $3}}' $inputFile)
+	
+	if [[ "$VCF1" == *gz ]] ;
+	then
+		VCF1Name=`basename $VCF1 .gz`
+	else
+		VCF1Name=`basename $VCF1`
+	fi
+	if [[ "$VCF2" == *gz ]] ;
+	then
+		VCF2Name=`basename $VCF2 .gz`
+	else
+		VCF2Name=`basename $VCF2`
+	fi
+	if [ "$VCF1Name" == "$VCF2Name" ]
+	then
+		VCF1BaseName=`basename $VCF1Name .vcf`
+		VCF2BaseName=`basename $VCF2Name .vcf`
+		VCFReName=${VCF1BaseName}_1.vcf
+		VCF1Name=${VCF1BaseName}_1.vcf
+		cp $VCF1 $OUTDIR/$RESULTDIR/$VCFReName
+		sed -i 's| (Variants)||g' $OUTDIR/$RESULTDIR/$VCFReName
+		sed -i 's| (paired)||g' $OUTDIR/$RESULTDIR/$VCFReName
+		VCFReName=${VCF2BaseName}_2.vcf
+		VCF2Name=${VCF2BaseName}_2.vcf
+		cp $VCF2 $OUTDIR/$RESULTDIR/$VCFReName
+		sed -i 's| (Variants)||g' $OUTDIR/$RESULTDIR/$VCFReName
+		sed -i 's| (paired)||g' $OUTDIR/$RESULTDIR/$VCFReName
+	else
+		cp $VCF1 $OUTDIR/$RESULTDIR/
+		sed -i 's| (Variants)||g' $OUTDIR/$RESULTDIR/$VCF1Name
+		sed -i 's| (paired)||g' $OUTDIR/$RESULTDIR/$VCFReName
+		cp $VCF2 $OUTDIR/$RESULTDIR/
+		sed -i 's| (Variants)||g' $OUTDIR/$RESULTDIR/$VCF2Name
+		sed -i 's| (paired)||g' $OUTDIR/$RESULTDIR/$VCFReName
+	fi
+	
+	
+	gunzip $OUTDIR/$RESULTDIR/*.vcf.gz
+	
+	VCF1=$OUTDIR/$RESULTDIR/$VCF1Name
+	VCF2=$OUTDIR/$RESULTDIR/$VCF2Name
+	
 	SAMPLE1=$(awk -F'\t' -v var=$x '{if(NR==var){print $4}}' $inputFile)
 	
 	SAMPLE2=$(awk -F'\t' -v var=$x '{if(NR==var){print $5}}' $inputFile)
+	
+	echo -e 'InterAssay\t'$OUTDIR'/'$RESULTDIR'/'$VCF1Name'\t'$OUTDIR'/'$RESULTDIR'/'$VCF2Name'\t'$SAMPLE1'\t'$SAMPLE2 >> $OUTDIR/$RESULTDIR/inputfile.tsv
+	
 	#CHECK VCF1
 	if [ ! -s "$VCF1" ] || [ ! -s "$VCF1" ]
 	then
@@ -113,19 +156,22 @@ do
 		echo "VCF1="$VCF1
 		echo "VCF2="$VCF2
 	fi
+	
 	TEST1=`${VCF_LIB}/vcfsamplenames $VCF1 |grep "$SAMPLE1"`
 	TEST2=`${VCF_LIB}/vcfsamplenames $VCF2 |grep "$SAMPLE2"`
-	Original_SampleName1=`${VCF_LIB}/vcfsamplenames $VCF1`
-	Original_SampleName2=`${VCF_LIB}/vcfsamplenames $VCF2`
+
 	if [ -z "$TEST1" ] || [ -z "$TEST2" ]
 	then
 		echo "Please check these sample names in these VCF Files!"
 		echo "VCF1=$VCF1 should be $SAMPLE1"
 		echo "VCF2=$VCF2 should be $SAMPLE2"
 	fi
+	Original_SampleName1=$SAMPLE1
+	Original_SampleName2=$SAMPLE2
 done
-#echo $SampleNameinInputVCF1
-#echo $SampleNameinInputVCF2
+
+inputFile=$OUTDIR/$RESULTDIR/inputfile.tsv
+
 ##################################################################################
 ###
 ###     Run Functions
@@ -137,54 +183,35 @@ do
 	LABEL=$(awk -F'\t' -v var=$x '{if(NR==var){print $1}}' $inputFile)
 	VCF1=$(awk -F'\t' -v var=$x '{if(NR==var){print $2}}' $inputFile)
 	VCF2=$(awk -F'\t' -v var=$x '{if(NR==var){print $3}}' $inputFile)
+	
 	SAMPLE1=$(awk -F'\t' -v var=$x '{if(NR==var){print $4}}' $inputFile)
 	SAMPLE2=$(awk -F'\t' -v var=$x '{if(NR==var){print $5}}' $inputFile)
-	VCF1Name=$(echo $VCF1 | rev | cut -f1 -d '/' | rev)
-	VCF2Name=$(echo $VCF2 | rev | cut -f1 -d '/' | rev)
-	if [ -f "$OUTDIR/$VCF1Name" ] && [ -f "$OUTDIR/$VCF2Name" ]; then continue; fi
-	Prefix1="1_"
-	Prefix2="2_"
-	if [ ! -f "$OUTDIR/$VCF1Name" ] && [ -f "$OUTDIR/$VCF2Name" ]; then VCF1Name=$Prefix1$VCF1Name VCF2Name=$VCF2Name; fi
-	if [ -f "$OUTDIR/$VCF1Name" ] && [ ! -f "$OUTDIR/$VCF2Name" ]; then VCF1Name=$VCF1Name VCF2Name=$Prefix2$VCF2Name; fi
-	if [ ! -f "$OUTDIR/$VCF1Name" ] && [ ! -f "$OUTDIR/$VCF2Name" ]; then VCF1Name=$Prefix1$VCF1Name VCF2Name=$Prefix2$VCF2Name; fi
-	if [ -z "$targetBed" ] ;
-	then
-		#Get a local copy of each VCF
-		if [[ "$VCF1" == *gz ]] ;
-		then
-			zcat $VCF1 > $OUTDIR/$VCF1Name
-		else
-			cp $VCF1 $OUTDIR/$VCF1Name
-		VCF1Loc=$(readlink -f $VCF1Name)
-		sed -i 's|'$VCF1'|'$VCF1Loc'|g' $inputFile
-		fi
-		if [[ "$VCF2" == *gz ]] ;
-		then
-			zcat $VCF2 > $OUTDIR/$VCF2Name
-		else
-			cp $VCF2 $OUTDIR/$VCF2Name
-		VCF2Loc=$(readlink -f $VCF2Name)
-		sed -i 's|'$VCF2'|'$VCF2Loc'|g' $inputFile
-		fi
-		
-		sed -i "s/$Original_SampleName1/$SAMPLE1/g" $OUTDIR/$VCF1Name
-		sed -i "s/$Original_SampleName2/$SAMPLE2/g" $OUTDIR/$VCF2Name
-	fi
+	
+	VCF1Name=`basename $VCF1 .vcf.gz`
+	VCF2Name=`basename $VCF2 .vcf.gz`
+
 	#If Target BED file is specified, restrict analysis to those regions
 	if [ ! -z "$targetBed" ] 
 	then	
-		$BEDTOOLS_PATH intersect -a $VCF1 -b $targetBed -u > tmp
-		grep "#" $VCF1 | sed "s/$Original_SampleName1/$SAMPLE1/g" > $OUTDIR/$VCF1Name
-		cat tmp >> $OUTDIR/$VCF1Name
-		VCF1Loc=$(readlink -f $VCF1Name)
-		sed -i 's|'$VCF1'|'$VCF1Loc'|g' $inputFile
-		$BEDTOOLS_PATH intersect -a $VCF2 -b $targetBed -u > tmp
-		grep "#" $VCF2 | sed "s/$Original_SampleName2/$SAMPLE2/g" > $OUTDIR/$VCF2Name
-		cat tmp >> $OUTDIR/$VCF2Name
-		VCF2Loc=$(readlink -f $VCF2Name)
-		sed -i 's|'$VCF2'|'$VCF2Loc'|g' $inputFile
+		$BEDTOOLS_PATH intersect -a $VCF1 -b $targetBed -u > $OUTDIR/$RESULTDIR/tmp
+		grep "#" $VCF1 > $OUTDIR/$RESULTDIR/${VCF1Name}_target.vcf
+		cat $OUTDIR/$RESULTDIR/tmp >> $OUTDIR/$RESULTDIR/${VCF1Name}_target.vcf
+		
+		$BEDTOOLS_PATH intersect -a $VCF2 -b $targetBed -u > $OUTDIR/$RESULTDIR/tmp
+		grep "#" $VCF2 > $OUTDIR/$RESULTDIR/${VCF2Name}_target.vcf
+		cat $OUTDIR/$RESULTDIR/tmp >> $OUTDIR/$RESULTDIR/${VCF2Name}_target.vcf
+		
+		echo -e 'InterAssay\t'$OUTDIR'/'$RESULTDIR'/'${VCF1Name}_target.vcf'\t'$OUTDIR'/'$RESULTDIR'/'${VCF2Name}_target.vcf'\t'$SAMPLE1'\t'$SAMPLE2 >> $OUTDIR/$RESULTDIR/inputfile_target.tsv	
 	fi
 done
 
+if [ ! -z "$targetBed" ]
+then
+	inputFile=$OUTDIR/$RESULTDIR/inputfile_target.tsv
+fi
+	
+
+
 $DIR/scripts/Run.sh $inputFile $OUTDIR/$RESULTDIR/ $DIR/scripts/ ${R_PATH} $DIR/jobs/
 echo "CONCORDANCE SUBMITTED"
+
